b810fc5ec9610fc04607ce48a119459ce2a1dc66
szajberpiraty <szajberpiraty@gmail.com>
2018-09-03 17:27:38 +0200

    #1 #5 #6
    Egy dimenziós tömbök kezelése

--- tomb_solution/tomb_1dim/Program.cs
+++ tomb_solution/tomb_1dim/Program.cs
@@ -16,7 +16,10 @@ namespace tomb_1dim
 		{
 			//Deklaráció, ez a tömb 30 integer típusú számot tud tárolni
 			int[] szamok=new int[30];
-				
+			int szamok_min=int.MaxValue;
+			int szamok_max=int.MinValue;
+			int szamok_osszeg=0;
+			double szamok_atlag;
 			
 			//Feltöltés adatokkal (véletlen számokkal ebben a példában)
 			Random veletlenszam=new Random();
@@ -30,6 +33,23 @@ namespace tomb_1dim
 				Console.WriteLine("Index:{0},elem:{1}",i,szamok[i]);
 			}
 			
+			//Minimum, maximum kiválasztás, összeg gyűjtése
+			for (int i = 0; i < szamok.Length; i++) {
+				
+				if (szamok[i]<szamok_min) {
+					szamok_min=szamok[i];
+				}
+				if (szamok[i]>szamok_max) {
+					szamok_max=szamok[i];
+				}
+				szamok_osszeg=szamok_osszeg+szamok[i];
+			}
+			//double-ként kell számolni a változókkal
+			szamok_atlag=(double)szamok_osszeg/(double)szamok.Length;
+			
+			Console.WriteLine("Darab:{0},Min:{1},Max:{2},Összeg:{3},Átlag:{4:0.000}"
+			                  ,szamok.Length,szamok_min,szamok_max
+			                  ,szamok_osszeg,szamok_atlag);
 			
 			Console.ReadLine();
 		}
