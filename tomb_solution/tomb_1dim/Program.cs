e99d049ee6c607cccf213f54fe41513b2185f97a
szajberpiraty <szajberpiraty@gmail.com>
2018-09-03 17:03:38 +0200

    #1
    Egy dimenziós tömb deklarációja, feltöltése, listázása

---
+++ tomb_solution/tomb_1dim/Program.cs
@@ -0,0 +1,37 @@
+﻿/*
+ * Készítette a SharpDevelop.
+ * Felhasználó: razgon
+ * Dátum: 2018. 09. 03.
+ * Idő: 16:47
+ * 
+ * A sablon megváltoztatásához használja az Eszközök | Beállítások | Kódolás | Szabvány Fejlécek Szerkesztését.
+ */
+using System;
+
+namespace tomb_1dim
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			//Deklaráció, ez a tömb 30 integer típusú számot tud tárolni
+			int[] szamok=new int[30];
+				
+			
+			//Feltöltés adatokkal (véletlen számokkal ebben a példában)
+			Random veletlenszam=new Random();
+			
+			for (int i = 0; i < szamok.Length; i++) {
+				szamok[i]=veletlenszam.Next(-100,100);
+			}
+			
+			//A tömb elemeinek kiíratása
+			for (int i = 0; i < szamok.Length; i++) {
+				Console.WriteLine("Index:{0},elem:{1}",i,szamok[i]);
+			}
+			
+			
+			Console.ReadLine();
+		}
+	}
+}
